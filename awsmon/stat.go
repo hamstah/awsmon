1399f3ba51361d68a74865a1abeb5f0b08a8d3d5
Ciro S. Costa <ciro.costa@liferay.com>
2017-07-13 21:56:27 -0300

    Adds proper metric sending routines

--- awsmon/stat.go
+++ awsmon/stat.go
@@ -1,21 +1,25 @@
 package main
 
+import (
+	"time"
+)
+
 type Stat struct {
-	Name      string
-	Unit      string
-	Value     float64
-	Timestamp time.Time
+	Name  string
+	Unit  string
+	Value float64
+	When  time.Time
 }
 
 // NewMemoryUtilizationStat generates a generic Stat
 // structure prefilled with information about memory
 // utilization
 func NewMemoryUtilizationStat(sample *MemorySample) Stat {
-	return &Stat{
-		Name:      "MemoryUtilization",
-		Unit:      "Percent",
-		Timestamp: sample.When,
-		Value:     sample.MemoryUtilization,
+	return Stat{
+		Name:  "MemoryUtilization",
+		Unit:  "Percent",
+		When:  sample.When,
+		Value: sample.MemoryUtilization,
 	}
 }
 
@@ -23,11 +27,11 @@ func NewMemoryUtilizationStat(sample *MemorySample) Stat {
 // structure prefilled with information about memory
 // utilization
 func NewSwapUtilizationStat(sample *MemorySample) Stat {
-	return &Stat{
-		Name:      "SwapUtilization",
-		Unit:      "Percent",
-		Timestamp: sample.When,
-		Value:     sample.SwapUtilization,
+	return Stat{
+		Name:  "SwapUtilization",
+		Unit:  "Percent",
+		When:  sample.When,
+		Value: sample.SwapUtilization,
 	}
 }
 
@@ -35,11 +39,11 @@ func NewSwapUtilizationStat(sample *MemorySample) Stat {
 // structure prefilled with information about disk
 // utilization.
 func NewDiskUtilizationStat(sample *DiskSample) Stat {
-	return &Stat{
-		Name:      "DiskUtilization",
-		Unit:      "Percent",
-		Timestamp: sample.When,
-		Value:     sample.DiskUtilization,
+	return Stat{
+		Name:  "DiskUtilization",
+		Unit:  "Percent",
+		When:  sample.When,
+		Value: sample.DiskUtilization,
 	}
 }
 
@@ -47,10 +51,10 @@ func NewDiskUtilizationStat(sample *DiskSample) Stat {
 // structure prefilled with information about inodes
 // utilization.
 func NewInodesUtilizationStat(sample *DiskSample) Stat {
-	return &Stat{
-		Name:      "InodesUtilization",
-		Unit:      "Percent",
-		Timestamp: sample.When,
-		Value:     sample.DiskUtilization,
+	return Stat{
+		Name:  "InodesUtilization",
+		Unit:  "Percent",
+		When:  sample.When,
+		Value: sample.DiskUtilization,
 	}
 }
